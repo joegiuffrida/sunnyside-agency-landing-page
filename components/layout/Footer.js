3b4b8cda65871a564e61f9141f75724a207ba300
Joe Giuffrida <conquercat24@gmail.com>
2022-01-09 09:53:38 -0800

    added footer with faux nav and social links

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,7 +1,30 @@
 import styles from './Footer.module.css';
 
 const Footer = () => {
-  return <div>Footer Goes Here</div>;
+  return (
+    <footer className={styles.footerContainer}>
+      <h2>sunnyside</h2>
+      <ul className={styles.footerLinks}>
+        <li>About</li>
+        <li>Services</li>
+        <li>Projects</li>
+      </ul>
+      <ul className={styles.footerSocial}>
+        <li>
+          <img src="/icon-facebook.svg" />
+        </li>
+        <li>
+          <img src="/icon-instagram.svg" />
+        </li>
+        <li>
+          <img src="/icon-twitter.svg" />
+        </li>
+        <li>
+          <img src="/icon-pinterest.svg" />
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
